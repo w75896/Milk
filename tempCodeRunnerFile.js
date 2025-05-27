021bff737dfa94bb0ad2fafea5b7adfb36b3326e
Your Name <you@example.com>
2025-05-27 16:00:31 +0800

    up

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+d INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    piglet_10kg REAL,
+    milk_purchase_ret REAL,
+    raw_sheep_milk REAL,
+    live_chicken REAL,
+    white_egg_b REAL
